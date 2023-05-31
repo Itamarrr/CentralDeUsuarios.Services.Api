9450aa62d1f6dbbb55f425da9b6b3628496b1da5
itamar santos de oliveira <itamar.oliveira@globalhitss.com.br>
2023-05-31 10:01:11 -0300

    Refatorando o codigo para eliminar redundancia na classe UsuarioRepository e deixando o codigo anterior comentado para conhecimento

--- CentralDeUsuarios.Services.Api/CentralDeUsuarios.Infra.Data/Repositories/UsuarioRepository.cs
+++ CentralDeUsuarios.Services.Api/CentralDeUsuarios.Infra.Data/Repositories/UsuarioRepository.cs
@@ -13,56 +13,61 @@ using System.Threading.Tasks;
 namespace CentralDeUsuarios.Infra.Data.Repositories
 {
     //classe para inplementar o repositorio de usuario
-    public class UsuarioRepository : IUsuarioRepository       
+    public class UsuarioRepository :BaseRepository<Usuario, Guid>, IUsuarioRepository       
     {
         //atributo
         private readonly SqlServerContext _sqlServerContext;
 
         //Construtor para injeção de dependencia
         public UsuarioRepository(SqlServerContext sqlServerContext)
+            : base(sqlServerContext) //base -> faz referencia a superclasse pois ela tambem tem que tr o  estou falando que esse oque vai ser injetado no construtor eu tambem quero que seja tambem injetado na classe pai
         {
             _sqlServerContext = sqlServerContext;
         }
-
-        public void Create(Usuario entity)
+        public Usuario? GetByEmail(string email)
         {
-            entity.Senha = MD5Helper.Encrypt(entity.Senha);
-            _sqlServerContext.Entry(entity).State = EntityState.Added;
-            //_sqlServerContext.Usuario.Add(entity); pode ser assim tambem
-            _sqlServerContext.SaveChanges();
+            return _sqlServerContext.Usuario.FirstOrDefault(u => u.Email.Equals(email));// traga o primeiro email que seja igual a esse emai
         }
+        //Comentato pois estou refatorando o codigo para herdar minha classe BaseRepository que já tem um crud
+        //public void Create(Usuario entity)
+        //{
+        //    entity.Senha = MD5Helper.Encrypt(entity.Senha);
+        //    _sqlServerContext.Entry(entity).State = EntityState.Added;
+        //    //_sqlServerContext.Usuario.Add(entity); pode ser assim tambem
+        //    _sqlServerContext.SaveChanges();
+        //}
 
-        public void Update(Usuario entity)
-        {
-            entity.Senha = MD5Helper.Encrypt(entity.Senha);
-            _sqlServerContext.Entry(entity).State = EntityState.Modified;           
-            _sqlServerContext.SaveChanges();
-        }
+        //public void Update(Usuario entity)
+        //{
+        //    entity.Senha = MD5Helper.Encrypt(entity.Senha);
+        //    _sqlServerContext.Entry(entity).State = EntityState.Modified;           
+        //    _sqlServerContext.SaveChanges();
+        //}
 
-        public void Delete(Usuario entity)
-        {
-            _sqlServerContext.Entry(entity).State = EntityState.Deleted;
-            //_sqlServerContext.Usuario.Remove(entity); pode ser assim tambem
-            _sqlServerContext.SaveChanges();
-        }
+        //public void Delete(Usuario entity)
+        //{
+        //    _sqlServerContext.Entry(entity).State = EntityState.Deleted;
+        //    //_sqlServerContext.Usuario.Remove(entity); pode ser assim tambem
+        //    _sqlServerContext.SaveChanges();
+        //}
 
-        public List<Usuario> GetAll()
-        {
-            return _sqlServerContext.Usuario.ToList();
-        }
-               
+        //public List<Usuario> GetAll()
+        //{
+        //    return _sqlServerContext.Usuario.ToList();
+        //}
 
-        public Usuario GetById(Guid id)
-        {
-            //Find sempre busca encima da chave primaria  id
-            return _sqlServerContext.Usuario.Find(id);
-        }
 
-       
+        //public Usuario GetById(Guid id)
+        //{
+        //    //Find sempre busca encima da chave primaria  id
+        //    return _sqlServerContext.Usuario.Find(id);
+        //}
 
-        public Usuario GetByEmail(string email)
-        {
-            return _sqlServerContext.Usuario.FirstOrDefault(u => u.Email.Equals(email));
-        }
+
+
+        //public Usuario GetByEmail(string email)
+        //{
+        //    return _sqlServerContext.Usuario.FirstOrDefault(u => u.Email.Equals(email));
+        //}
     }
 }
