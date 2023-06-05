c8e7b0f859ba8b0f3d46768b24f4b0248ad8462d
itamar santos de oliveira <itamar.oliveira@globalhitss.com.br>
2023-06-05 19:07:40 -0300

    Criada a class UsuarioAppService herdando IUsuarioAppService com criação do contrutor para IUsuarioDomainServices

---
+++ CentralDeUsuarios.Services.Api/CentralDeUsuarios.Aplication/Services/UsuarioAppService.cs
@@ -0,0 +1,35 @@
+﻿using CentralDeUsuario.Domain.Interfaces.Services;
+using CentralDeUsuarios.Aplication.Commands;
+using CentralDeUsuarios.Aplication.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralDeUsuarios.Aplication.Services
+{
+    //implementação dos serviços de aplicação
+    public class UsuarioAppService : IUsuarioAppService
+    {
+        //readonly é usuario para  quando é construtor esta abaixo a primeira dependencia
+        //pra isso tive que fazer referencia ao projeto CentralDeUsuarioDomain 
+        private readonly IUsuarioDomainServices _usuarioDomainServices;
+
+        public UsuarioAppService(IUsuarioDomainServices usuarioDomainServices)
+        {
+            _usuarioDomainServices = usuarioDomainServices;
+        }
+
+        public void CriarUsuario(CriarUsuarioCommand command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            //Dispose para limpar sujeiras ou seja aquilo que injeta como dependencia depois joga fora
+            _usuarioDomainServices.Dispose();
+        }
+    }
+}
