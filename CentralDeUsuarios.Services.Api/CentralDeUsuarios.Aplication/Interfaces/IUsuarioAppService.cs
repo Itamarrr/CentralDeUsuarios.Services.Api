be29a847756fb5a4f5a15552c31b245bee995c8d
itamar santos de oliveira <itamar.oliveira@globalhitss.com.br>
2023-06-05 18:39:43 -0300

    Criada a interface IUsuarioAppService com a assinatura do seguinte contrato CriarUsuarioCommand

---
+++ CentralDeUsuarios.Services.Api/CentralDeUsuarios.Aplication/Interfaces/IUsuarioAppService.cs
@@ -0,0 +1,17 @@
+﻿using CentralDeUsuarios.Aplication.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralDeUsuarios.Aplication.Interfaces
+{
+    //interface para abstração dos metodos da camada de aplicação paa usúario
+    public interface IUsuarioAppService : IDisposable //IDisposable essa interface foi implementada para limpar sujeiras
+    {
+        //esse é o contrato de dados que temos que passar para esse metodo
+        // isso que é intregado para API
+        void CriarUsuario(CriarUsuarioCommand command);// Método para criar um usuario na aplicação ele recebe os dados atravez do commmand
+    }
+}
