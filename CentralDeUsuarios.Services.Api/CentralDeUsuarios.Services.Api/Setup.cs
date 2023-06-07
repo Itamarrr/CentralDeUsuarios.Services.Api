8093bb44e9fac0714d7033dc717a66b3bb5e3616
itamar santos de oliveira <itamar.oliveira@globalhitss.com.br>
2023-06-06 22:02:17 -0300

    incluido os parametros para configuração do appsettings.json

--- CentralDeUsuarios.Services.Api/CentralDeUsuarios.Services.Api/Setup.cs
+++ CentralDeUsuarios.Services.Api/CentralDeUsuarios.Services.Api/Setup.cs
@@ -1,6 +1,8 @@
 ﻿namespace CentralDeUsuarios.Services.Api
 {
+    //Essa classe é para configura minha injeção de dependencia do AspNet para ser acionada assim que rodar
     public class Setup
     {
+        
     }
 }
