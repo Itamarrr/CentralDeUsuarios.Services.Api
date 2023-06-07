97fb2e3da3e6979db4dfcde4dc6cd7b7ebf78c8b
itamar santos de oliveira <itamar.oliveira@globalhitss.com.br>
2023-06-07 10:15:29 -0300

    Craido a classe CommandToEntityMap.cs e instalado os pacotes AutoMapper AutoMapper extensions for ASP.NET Core

---
+++ CentralDeUsuarios.Services.Api/CentralDeUsuarios.Aplication/Mappings/CommandToEntityMap.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using CentralDeUsuario.Domain.Entities;
+using CentralDeUsuarios.Aplication.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralDeUsuarios.Aplication.Mappings
+{
+    //esse nome é porque eu pego os objeto do comand para o objeto map
+    //ele Classe de Mapeamento do AutoMapper ele se[mpre herda o Projile
+    public class CommandToEntityMap: Profile
+    {
+        public CommandToEntityMap() {
+            CreateMap<CriarUsuarioCommand, Usuario>()//passar desse CriarUsuarioCommand para esse Usuario
+                                                     //Como tem no usuario Id e o campo DataHoraDeCriacao eu posso fazer assim
+                .AfterMap((command,entity )=>
+                {
+                    entity.Id = Guid.NewGuid();// gerando o novo id
+                    entity.DataHoraDeCriacao = DateTime.Now; // gerando a data e hora atual 
+                });
+            
+            
+        }
+    }
+}
