d8b25a7f4cc035db65f44c0383e21cae0f0b911f
itamar santos de oliveira <itamar.oliveira@globalhitss.com.br>
2023-06-07 11:21:15 -0300

    // injetando dependencias AddMessageServices // injetando dependencias AddMessageServices

--- CentralDeUsuarios.Services.Api/CentralDeUsuarios.Services.Api/Setup.cs
+++ CentralDeUsuarios.Services.Api/CentralDeUsuarios.Services.Api/Setup.cs
@@ -2,18 +2,53 @@
 using CentralDeUsuario.Domain.Interfaces.Services;
 using CentralDeUsuarios.Aplication.Interfaces;
 using CentralDeUsuarios.Aplication.Services;
+using CentralDeUsuarios.Infra.Data.Contexts;
 using CentralDeUsuarios.Infra.Data.Repositories;
+using CentralDeUsuarios.Infra.Messages.Helpers;
+using CentralDeUsuarios.Infra.Messages.Producers;
+using CentralDeUsuarios.Infra.Messages.Settings;
+using Microsoft.EntityFrameworkCore;
 
 namespace CentralDeUsuarios.Services.Api
 {
     //Essa classe é para configura minha injeção de dependencia do AspNet para ser acionada assim que rodar
     public static class Setup
     {
+        // injetando dependencias AddRegisterServices
         public static void AddRegisterServices(this WebApplicationBuilder builder)
         {
             builder.Services.AddTransient<IUsuarioAppService, UsuarioAppService>();// injetando a interface IUsuarioAppService na classe UsuarioAppService
             builder.Services.AddTransient<IUsuarioDomainServices, UsuarioDomainServices>();// injetando a interface IUsuarioDomainServices na classe UsuarioDomainServices
             builder.Services.AddTransient<IUsuarioRepository, UsuarioRepository>();// injetando a interface IUsuarioRepository na classe IUsuarioRepository
+
         }
+
+        // injetando dependencias AddEntityFramework
+        public static void AddEntityFramework(this WebApplicationBuilder builder)
+        {
+            var connectionString = builder.Configuration.GetConnectionString("CentralDeUsuarios");//  pegando a conection string
+            builder.Services.AddDbContext<SqlServerContext>(options => options.UseSqlServer(connectionString));
+        }
+
+        // injetando dependencias AddMessageServices
+        public static void AddMessageServices(this WebApplicationBuilder builder)
+        {
+            builder.Services.Configure<MessageSettings>(builder.Configuration.GetSection("MessageSettings"));//
+            builder.Services.Configure<MessageSettings>(builder.Configuration.GetSection("MailSettings"));//
+
+            builder.Services.AddTransient<MensageQueueProducer>();
+            builder.Services.AddTransient<EmailHelper>();
+
+
+
+        }
+
+        // injetando dependencias AddMessageServices
+        public static void AddAutoMapperServices(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+        }
+
+
     }
 }
