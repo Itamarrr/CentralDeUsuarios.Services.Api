83eea88ec61936632efe2980d8a51ed960aaec7f
itamar santos de oliveira <itamar.oliveira@globalhitss.com.br>
2023-06-06 22:27:50 -0300

    criada a classe MessageSettings para servidor de mensageria e a classe MailSettings para servidor de email

--- CentralDeUsuarios.Services.Api/CentralDeUsuarios.Infra.Messages/Settings/MessageSettings.cs
+++ CentralDeUsuarios.Services.Api/CentralDeUsuarios.Infra.Messages/Settings/MessageSettings.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace CentralDeUsuarios.Infra.Messages.Settings
 {
-    internal class MessageSettings
+    //Configurações para conexão do servidor de mensageria
+    public class MessageSettings
     {
+        public string? Host { get; set; }
+        public string? Queue { get; set; }
+        public string? Username { get; set; }
+        public string? Password { get; set; }
     }
 }
