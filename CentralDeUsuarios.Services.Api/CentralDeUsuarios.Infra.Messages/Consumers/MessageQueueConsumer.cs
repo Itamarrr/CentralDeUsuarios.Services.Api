b73ff0a6258cdb89894b71139afd091ed9ccc000
itamar santos de oliveira <itamar.oliveira@globalhitss.com.br>
2023-06-09 15:19:00 -0300

    Criado a clesse MessageQueueConsumer para Ler Consumir e disparar as mensagens da fila. Faoi criado o metod construtor com alguns parametros para o mesmo

---
+++ CentralDeUsuarios.Services.Api/CentralDeUsuarios.Infra.Messages/Consumers/MessageQueueConsumer.cs
@@ -0,0 +1,22 @@
+﻿using CentralDeUsuarios.Infra.Messages.Helpers;
+using CentralDeUsuarios.Infra.Messages.Settings;
+using Microsoft.Extensions.Hosting;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralDeUsuarios.Infra.Messages.Consumers
+{
+    //classe para consumir e processar as mensagens da fila pra isso tenho que herda a clesse BackgroundService
+    public class MessageQueueConsumer: BackgroundService
+    {
+        private readonly MessageSettings? _messageSettings;// injetando de dependencia para se ler os dados para poder conectar na fila 
+        private readonly IConnection? _connection;// conectar na fila 
+        private readonly IModel? _model;// para ler as menssagens na fula 
+        private readonly IServiceProvider? _serviceProvider;// para trabalhar com o consumer
+        private readonly EmailHelper? _emailHelper;// para disparar o email
+    }
+}
