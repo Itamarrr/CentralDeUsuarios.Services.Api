a6583db0677bca9246e84e6090378e9db68ca22f
itamar santos de oliveira <itamar.oliveira@globalhitss.com.br>
2023-06-09 16:07:51 -0300

    //Fazer a leitura pra isso tenho que capiturar o sender e um  args criado switch

--- CentralDeUsuarios.Services.Api/CentralDeUsuarios.Infra.Messages/Consumers/MessageQueueConsumer.cs
+++ CentralDeUsuarios.Services.Api/CentralDeUsuarios.Infra.Messages/Consumers/MessageQueueConsumer.cs
@@ -1,9 +1,13 @@
 ﻿using CentralDeUsuarios.Infra.Messages.Helpers;
+using CentralDeUsuarios.Infra.Messages.Models;
 using CentralDeUsuarios.Infra.Messages.Settings;
 using CentralDeUsuarios.Infra.Messages.ValueObjects;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
 using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -55,9 +59,49 @@ namespace CentralDeUsuarios.Infra.Messages.Consumers
         //Metodo para ler a fila do RabbitMQ
         protected override Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            throw new NotImplementedException();
+            // COMPONENTE PARA FAZER A LEITURA DA FILA EventingBasicConsumer classe que ler e processa minha fila
+            var consumer = new EventingBasicConsumer(_model);
+            //Fazer a leitura pra isso tenho que capiturar o sender e um  args
+            consumer.Received += (sender, args) =>
+            {
+                 //ler um conteudo gravada na fla 
+                 //Prara isso eu pego ela em bytes deposi converto para string
+                 var contentArray = args.Body.ToArray(); // pegando em bytes 
+                var contentString = Encoding.UTF8.GetString(contentArray);  // convertendo em string
+
+                //Como vem em json tenho que deserializar pois vem em jsom
+                var messageQueuModel = JsonConvert.DeserializeObject<MensageQueueModel>(contentString);
+
+                //verificar o tipo de mensagem 
+                switch (messageQueuModel.Tipo)
+                {
+                    case TipoMensagem.CONFIRMACAO_DE_CADASTRO:
+                        //CreateScope ao ler a mensagem vai trar da fila
+                        using (var scope = _serviceProvider.CreateScope())
+                        {
+                            //capiturar os usuario contido na mensagem
+                            var messageMenssageVO= JsonConvert.DeserializeObject<UsuariosMessageVO>(messageQueuModel.Conteudo);
+                            //enviar email agora
+                            EnviarMensagemDeConfirmacaoDeCadastro(messageMenssageVO);
+                            //Comunicar ao Rabbit que a menssagem foi processada para retirar a mensagem da fila
+                            _model.BasicAck(args.DeliveryTag, false);// eu falo mensagem foi processada pode retirar ela da fila
+                        }
+
+                        break;
+
+                    case TipoMensagem.RECUPERACAO_DE_SENHA:
+                        // todo aindda tenho que fazer
+                        break;
+                }
+
+
+            };
+            //executando o consumidor
+            _model.BasicConsume(_messageSettings.Queue, false, consumer);
+            return Task.CompletedTask;//CompletedTask informa qu a Received finalizou que não deu erro
+
         }
-        
+
 
         //Metodo para escrever e enviar o email de confirmação de cadastro de usaurio
         private void EnviarMensagemDeConfirmacaoDeCadastro(UsuariosMessageVO usuariosMessageVO)
